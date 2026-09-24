2fea52d8d6de2e9b93591bcfcf9dad961aa9ae49
Malia Lockhart <mlockhart@smith.edu>
2026-09-24 19:17:46 -0400

    Parts 1-4

--- hw01.py
+++ hw01.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------------
-#        Name: (put your name here)
+#        Name: Malia Lockhart
 #       Peers: (add any collaborators)
 #  References: (anything you checked to solve this)
 # ------------------------------------------------------
@@ -45,6 +45,20 @@ def main():
     # Part 1: Basic Operations
     # =============================================
     # Your code for part 1 under this line and before the print statements
+    x = 27
+    y = 1
+    a = 1.5
+    b = 7
+    c = -1
+    result1 = ((3*x - 9*y)/(2*a*(b-c)))
+    
+    print("Part 1: x =", 27)
+    print("Part 1: y =", y)
+    print("Part 1: a =", a)
+    print("Part 1: b =", b)
+    print("Part 1: c =", c)
+    print("Part 1: result =", result1)
+
 
     # End of Part 1 ----------------------
 
@@ -52,7 +66,13 @@ def main():
     # Part 2: Power
     # =============================================
     # Your code for part 2 under this line and before the print statements
-
+    x = 5
+    y = -3
+    result2 = (x**2*y**4)
+    
+    print ("Part 2: x =", x)
+    print ("Part 2: y =", y)
+    print ("Part 2: result =", result2)
     # End of Part 2 ----------------------
 
 
@@ -60,7 +80,13 @@ def main():
     # Part 3: Integer divide
     # =============================================
     # Your code for part 3 under this line and before the print statements
-
+    a = 100
+    b = 13
+    result3 = a//b
+    
+    print ("Part 3: a =", a)
+    print ("Part 3: b =", b)
+    print ("Part 3: result =", result3)
     # End of Part 3 ----------------------
 
 
